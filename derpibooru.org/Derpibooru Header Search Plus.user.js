188cc25375061f8a21885af2851d3c6bee2740b2
ChristianSilvermoon <ChristianSilvermoon@users.noreply.github.com>
2018-08-28 00:35:24 -0700

    Corrected version number

--- derpibooru.org/Derpibooru Header Search Plus.user.js
+++ derpibooru.org/Derpibooru Header Search Plus.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://christiansilvermoon.github.io/UserScripts/
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAVlJREFUWIXFl81xgzAQhZ8YysgxhaSDNJDUgqnFueXkDtxBGvDRuaSJzcFSWJC0b8EyeTPMYGal97E/YhwACB6n4AkQ4NTQ8xPAFcDZBdFPtz8NIZJOAF7Fgujn5t+NAb4oRDfdtjZfQpR7rXusOYfostidIXYEKEPsDJBD9GZsTTLkz8JYCDzQneJBRAPrxhnIO24HkUfnFRnwmAOAHCOETz4AT8p1jBwrJcnFm9Bb7zBOF1uvtL4Jk8FyY228jJGhmhEbwDIp/V4qjKY5B2DSgDUTArkdQG/snZCmAO6xtEvADyLWB8zcXHfY8C3wvHmKSaYGNAcoLS5ByDBdbL2SrwfSODEIp6mWvwTeTdf0CNZOQe0U3GC8DeBOMx9ArbbsG1DLChnFeA68AXjmuE11AfChM3CZQ1h1tt62NDF67Z9XfAxAgBcAT272Nrr9f+znD/ZXzMD/6Rf2NngPzEexFgAAAABJRU5ErkJggg==
 // @description Improves Derpibooru's Search Header with features from the search box at the bottom of the page.
-// @version     18.30.7
+// @version     18.7.30
 // @grant       none
 // @include     https://derpibooru.org*
 // ==/UserScript==
